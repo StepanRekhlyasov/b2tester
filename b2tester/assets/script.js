7900231b81a0b7112dd6f2ec66f6d324dc608fa6
Stepan <srekhlyasov@gmail.com>
2023-04-13 19:11:23 +0300

    incapculating

--- b2tester/assets/script.js
+++ b2tester/assets/script.js
@@ -1,22 +1,24 @@
-class B2Tester {
-	fetchPosts = () => {
-		$.ajax({
-			url: "/ajax.php",
-			method: "POST",
-			data: { action : 'b2tester_fetch', mode: 'native' }
-		}).done((data) => {
-			this.render(data)
-		})
+(function($) {
+	class B2Tester {
+		fetchPosts = () => {
+			$.ajax({
+				url: "/ajax.php",
+				method: "POST",
+				data: { action : 'b2tester_fetch', mode: 'native' }
+			}).done((data) => {
+				this.render(data)
+			})
+		}
+		render = (data) => {
+			data.forEach((row, index)=>{
+				if(row.post_content){
+					$('body').append('<div style="margin:50px;">'+row.post_content+'</div>')
+				}
+			})
+		}
 	}
-	render = (data) => {
-		data.forEach((row, index)=>{
-			if(row.post_content){
-				$('body').append('<div style="margin:50px;">'+row.post_content+'</div>')
-			}
-		})
-	}
-}
-$(document).on('ready', ()=>{
-	const b2tester = new B2Tester();
-	b2tester.fetchPosts()
-})
+	$(document).on('ready', ()=>{
+		const b2tester = new B2Tester();
+		b2tester.fetchPosts()
+	})
+})(jQuery);
