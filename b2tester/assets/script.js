30a98dd817fabc0f725760ea7323135fa4a16135
Stepan <srekhlyasov@gmail.com>
2023-04-13 18:24:20 +0300

    1

---
+++ b2tester/assets/script.js
@@ -0,0 +1,22 @@
+class B2Tester {
+	fetchPosts = () => {
+		$.ajax({
+			url: "/ajax.php",
+			method: "POST",
+			data: { action : 'b2tester_fetch', mode: 'native' }
+		}).done((data) => {
+			this.render(data)
+		})
+	}
+	render = (data) => {
+		data.forEach((row, index)=>{
+			if(row.post_content){
+				$('body').append('<div style="margin:50px;">'+row.post_content+'</div>')
+			}
+		})
+	}
+}
+$(document).on('ready', ()=>{
+	const b2tester = new B2Tester();
+	b2tester.fetchPosts()
+})
